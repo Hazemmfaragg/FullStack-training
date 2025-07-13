fc325cf2aa1058b1eb6a41267b456703327e6164
Hazemmfaragg <158563811+Hazemmfaragg@users.noreply.github.com>
2025-07-14 02:00:31 +0300

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body  style="background-color: aquamarine">
+
+<div class="container mn-4">
+<div class="row mt-4 p-5 justify-content-center mn-4">
+<div class="col-lg-4 mn-4">
+    
+<form class="w-100 bg-light p-5">
+    <h1 class="text-center mn-4">User Profile</h1>
+    <div class="alert alert-success mt-4 mb-4"><b>welcome,Hazem Farag!</b> </div>
+    <div class="card" >
+        <div class="card-header">
+        <h4>User Information</4>
+        </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item"><b>Name</b>: hazem farag</li>
+    <li class="list-group-item"><b>Email</b>: hazem@gmail.com</li>
+    <li class="list-group-item"><b>Age</b>: 21</li>
+    <li class="list-group-item"><b>City</b>: Mansoura</li>
+    <div class="d-grid gap-2" style="margin-top:5px">
+</div>
+  </ul>
+</div>
+<a href="task1.php" class="btn btn-primary mt-4 " style="width:100%">Back to Form</a>
+</form>
+</div>
+</div>
+</div>
+<script src="js/bootstrap.bundle.js"></script>
+</body>
+<html>
